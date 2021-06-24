723e58e8c0f330308c76a207218bea10f804a382
Melike Beyza BAKICI <46328844+beyzabakici@users.noreply.github.com>
2021-06-24 13:02:56 +0300

    Complate map screen & bottom tab navigator

---
+++ src/app/pages/MainMap.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+import MapView, { PROVIDER_DEFAULT } from 'react-native-maps';
+
+export function MainMap() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}
+    >
+      <MapView
+        provider={PROVIDER_DEFAULT}
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 41.0391643,
+          longitude: 28.9982707,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </SafeAreaView>
+
+  );
+}
