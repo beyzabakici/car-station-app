0e7dcde305d18a1a810e1574b0c0877d549b9121
Melike Beyza BAKICI <46328844+beyzabakici@users.noreply.github.com>
2021-06-28 00:29:27 +0300

    Add maps marker

--- src/pages/MapScreen.js
+++ src/pages/MapScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
-import MapView, { PROVIDER_DEFAULT } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import Loading from '../components/Loading';
 
@@ -8,6 +8,13 @@ export default function MapScreen() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [isLoading, setLoading] = useState(false);
+
+  const coordFede = {
+    latitude: 38.0157826,
+    longitude: 32.5221166,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  };
 
   useEffect(() => {
     setLoading(true);
@@ -35,15 +42,16 @@ export default function MapScreen() {
     <SafeAreaView style={styles.container} >
       {!isLoading && location
         ? <MapView
-          provider={PROVIDER_DEFAULT}
+          provider={PROVIDER_GOOGLE}
           style={{ flex: 1 }}
           initialRegion={{
             latitude: location.latitude,
             longitude: location.longitude,
             latitudeDelta: 0.01,
             longitudeDelta: 0.01,
-          }}
-        />
+          }}>
+            <Marker coordinate={ coordFede } />
+          </MapView>
         : <Loading />
       }
     </SafeAreaView>
